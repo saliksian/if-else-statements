f0508f9ba29c3361a7bcf6a9fe05288c653e404f
saliksian <salikafzal91@gmail.com>
2021-12-04 23:43:58 +0500

    Program7
    Character is Alphabet or Not

---
+++ Program7/script.js
@@ -0,0 +1,16 @@
+function alphaBet() {
+  var dom = document;
+  var char = dom.getElementById("char");
+  var char1 = parseInt(char.value)
+
+ 
+
+  var result = dom.getElementById("result");
+  console.log(char1);
+
+if (condition) {
+  
+}
+
+  char.value = "";
+}
