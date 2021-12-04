0c9f6766699dc66cabe0cfd141c9cec3a09b86d6
saliksian <salikafzal91@gmail.com>
2021-12-05 00:04:56 +0500

    Program7
    updated Program7

--- Program7/script.js
+++ Program7/script.js
@@ -1,15 +1,19 @@
 function alphaBet() {
   var dom = document;
   var char = dom.getElementById("char");
-  var char1 = parseInt(char.value)
+  var char1 = char.value
 
  
 
   var result = dom.getElementById("result");
   console.log(char1);
 
-if (condition) {
-  
+if (char1 >= 'A' || char <'Z' || char == 'Z') {
+  result.innerHTML = `${char.value} : is an Alphabet`;
+
+}else{
+  result.innerHTML = `${char.value} : is Not an Alphabet`;
+
 }
 
   char.value = "";
