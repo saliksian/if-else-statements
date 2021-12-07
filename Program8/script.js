81ce2864dbf7cb0982c30636ee2550db5534d3b5
saliksian <salikafzal91@gmail.com>
2021-12-07 20:09:26 +0500

    Program8 update
    Program to check vowel or consonant

--- Program8/script.js
+++ Program8/script.js
@@ -1,30 +1,36 @@
 function vowelOrNot() {
   var dom = document;
   var char = dom.getElementById("char");
-  var char1 = char.value
-
- 
+  var char1 = char.value;
 
   var result = dom.getElementById("result");
   // console.log(char1.length);
 
   if (char1.length == 1) {
-    // || char <'Z' || char == 'Z'
-    if (char1 >= 'A' ) {
-      result.innerHTML = `${char.value} : is an Alphabet`;
-    
-    }else{
+    if (char1 >= "A") {
+      // result.innerHTML = `${char.value} : is an Alphabet`;
+      if (
+        char1 == "A" ||
+        char1 == "a" ||
+        char1 == "E" ||
+        char1 == "e" ||
+        char1 == "I" ||
+        char1 == "i" ||
+        char1 == "O" ||
+        char1 == "o" ||
+        char1 == "U" ||
+        char1 == "u"
+      ) {
+        result.innerHTML = `${char.value} : is a Vowel`;
+      } else {
+        result.innerHTML = `${char.value} : is Consonant`;
+      }
+    } else {
       result.innerHTML = `${char.value} : is Not an Alphabet`;
-      
     }
-    
-  }
-  else{
+  } else {
     result.innerHTML = `Enter a Single Character!!`;
-
   }
-
-
 
   char.value = "";
 }
